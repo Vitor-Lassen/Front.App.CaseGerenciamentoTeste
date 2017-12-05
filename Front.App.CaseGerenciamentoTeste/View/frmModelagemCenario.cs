e0a8a64c1ad8c8186020b0d7b96dac71f6d6f746
Vitor Lassen <vitor@lassen.com.br>
2017-12-05 09:43:46 -0200

    modelagem

--- Front.App.CaseGerenciamentoTeste/View/frmModelagemCenario.cs
+++ Front.App.CaseGerenciamentoTeste/View/frmModelagemCenario.cs
@@ -95,7 +95,6 @@ namespace Front.App.CaseGerenciamentoTeste.View
             txtCodCen.Text = _cen.cod_cen.ToString();
             txtDesc.Text = _cen.descri_cen;
             txtNomeCen.Text = _cen.nome_cen;
-        
 
             _proj = JsonConvert.DeserializeObject<Projeto>(api.Get("api/projeto/select/all/"+_cen.cod_proj_cen));
             cboProj.SelectedIndex = cboProj.FindStringExact(_proj.nome_proj);
